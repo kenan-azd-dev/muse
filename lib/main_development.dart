3063714c14003299679ff315f40b3b53f70293e1
Kenan Azd <kenanazd.dev@gmail.com>
2024-04-08 18:44:37 +0300

    added main file for development

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,7 +1,8 @@
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
 // 3rd Party Packages
-import 'package:firebase_app_check/firebase_app_check.dart';
+// import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -22,13 +23,14 @@ void main() async {
   if (kDebugMode) {
     try {
       String localhost = '192.168.1.104';
-      await FirebaseAppCheck.instance.activate(
-        webProvider: ReCaptchaV3Provider('recaptcha-v3-site-key'),
-        androidProvider: AndroidProvider.debug,
-        appleProvider: AppleProvider.debug,
-      );
-      FirebaseFirestore.instance.useFirestoreEmulator(localhost, 8080, automaticHostMapping: false);
-      await FirebaseAuth.instance.useAuthEmulator(localhost, 9099, automaticHostMapping: false);
+      // await FirebaseAppCheck.instance.activate(
+      //   webProvider: ReCaptchaV3Provider('recaptcha-v3-site-key'),
+      //   androidProvider: AndroidProvider.debug,
+      //   appleProvider: AppleProvider.debug,
+      // );
+      FirebaseFirestore.instance.useFirestoreEmulator(localhost, 8080);
+      await FirebaseAuth.instance.useAuthEmulator(localhost, 9099);
+      FirebaseStorage.instance.useStorageEmulator(localhost, 9199);
     } catch (e) {
       // ignore: avoid_print
       print(e);
